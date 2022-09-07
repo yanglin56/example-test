d886f430d4a715cba2049237e8d6ea40a1464c3f
yanglin <yanglin556@yahoo.com>
2022-09-07 22:05:22 +0800

    deploy

---
+++ precache-manifest.79cfec7903872aacae3431ffd9d94040.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f72faf89f09c51bfc86",
+    "url": "/example/css/app.0a704115.css"
+  },
+  {
+    "revision": "f60738a72217f556f4cd83a7cb6c871e",
+    "url": "/example/index.html"
+  },
+  {
+    "revision": "7f72faf89f09c51bfc86",
+    "url": "/example/js/app.66e9c80e.js"
+  },
+  {
+    "revision": "1a0aab69d03104f21033",
+    "url": "/example/js/chunk-vendors.7d6a0176.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/example/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/example/robots.txt"
+  }
+]);
